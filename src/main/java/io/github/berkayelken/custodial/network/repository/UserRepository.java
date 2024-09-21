fabb81ad40ae4dcdd4353c8d4983813640ee2caa
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 14:15:55 +0300

    Create UserRepository.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package io.github.berkayelken.custodial.network.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserRepository, String> {
+}
