5bf5c55e4ea6cb50fe89e0a5c4378fc4f1eeb228
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 13:45:41 +0300

    custodial network init

---
+++ src/main/java/io/github/berkayelken/custodial/network/MsCustodialNetworkApplication.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.custodial.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustodialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustodialNetworkApplication.class, args);
+	}
+
+}
