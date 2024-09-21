7deb9ba3c22a8a6b165709b7117045d0e66d09a3
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 20:04:37 +0300

    findByEmail form db

--- src/main/java/io/github/berkayelken/custodial/network/repository/UserRepository.java
+++ src/main/java/io/github/berkayelken/custodial/network/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package io.github.berkayelken.custodial.network.repository;
 
+import io.github.berkayelken.custodial.network.domain.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserRepository, String> {
+public interface UserRepository extends CrudRepository<UserEntity, String> {
+	UserEntity findByEmail(String email);
 }
