98076e6eac53041b196e8787e8cf243812f6b2bf
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 18:20:15 +0300

    Update UserEntityTest.java

--- src/test/java/io/github/berkayelken/custodial/network/domain/UserEntityTest.java
+++ src/test/java/io/github/berkayelken/custodial/network/domain/UserEntityTest.java
@@ -1,5 +1,6 @@
 package io.github.berkayelken.custodial.network.domain;
 
+import io.github.berkayelken.custodial.network.domain.auth.UserType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.util.UUID;
 
 public class UserEntityTest {
 	private static final String TEST_STR = UUID.randomUUID().toString();
+	private static final int DEFAULT_INT = 0;
 
 	@Test
 	public void testNoArgsConstructor() {
@@ -20,6 +22,7 @@ public class UserEntityTest {
 		Assertions.assertNull(entity.getId());
 		Assertions.assertNull(entity.getEmail());
 		Assertions.assertNull(entity.getPassword());
+		Assertions.assertEquals(DEFAULT_INT, entity.getRole());
 	}
 
 	@Test
@@ -29,9 +32,11 @@ public class UserEntityTest {
 		entity.setId(TEST_STR);
 		entity.setEmail(TEST_STR);
 		entity.setPassword(TEST_STR);
+		entity.setRole(UserType.ARTIST.getValidOperations());
 
 		Assertions.assertEquals(TEST_STR, entity.getId());
 		Assertions.assertEquals(TEST_STR, entity.getEmail());
 		Assertions.assertEquals(TEST_STR, entity.getPassword());
+		Assertions.assertEquals(UserType.ARTIST.getValidOperations(), entity.getRole());
 	}
 }
