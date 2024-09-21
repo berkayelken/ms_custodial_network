6286d93d2524583e9602f8cc43f6225a5a6cb5d8
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 19:06:53 +0300

    UserType finder

--- src/main/java/io/github/berkayelken/custodial/network/domain/auth/UserType.java
+++ src/main/java/io/github/berkayelken/custodial/network/domain/auth/UserType.java
@@ -3,12 +3,25 @@ package io.github.berkayelken.custodial.network.domain.auth;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 @AllArgsConstructor
 public enum UserType {
-	COLLECTOR("ROLE_COLLECTOR",1),
-	ARTIST("ROLE_ARTIST",3);
+	COLLECTOR("ROLE_COLLECTOR", 1),
+	ARTIST("ROLE_ARTIST", 3);
 
 	private final String authorityRole;
 	private final int validOperations;
+
+	public static UserType findUserType(String authorityRole) {
+		return Arrays.stream(UserType.values()).filter(type -> authorityRole.equalsIgnoreCase(type.authorityRole)).findFirst()
+				.orElse(COLLECTOR);
+	}
+
+	public static UserType findUserType(int validOperations) {
+		return Arrays.stream(UserType.values()).filter(type -> validOperations == type.validOperations).findFirst()
+				.orElse(COLLECTOR);
+	}
+
 }
