1ff60baf8d50f04ce9fc645a0a052ca5507d12e1
Berkay Yelken <berkayelken@gmail.com>
2024-09-21 18:20:18 +0300

    Update UserEntity.java

--- src/main/java/io/github/berkayelken/custodial/network/domain/UserEntity.java
+++ src/main/java/io/github/berkayelken/custodial/network/domain/UserEntity.java
@@ -1,16 +1,19 @@
 package io.github.berkayelken.custodial.network.domain;
 
+import io.github.berkayelken.custodial.network.domain.auth.UserType;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
 @Document(collation = "user")
 public class UserEntity {
-	@MongoId
+	@MongoId(targetType = FieldType.STRING)
 	private String id;
 	private String email;
 	private String password;
+	private int role;
 }
