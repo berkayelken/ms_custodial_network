f3b50ddede482f344d6824f97a63cbd34314f468
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 16:41:54 +0300

    Create CrossMintConstants.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/CrossMintConstants.java
@@ -0,0 +1,12 @@
+package io.github.berkayelken.custodial.network.domain.cross.mint;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CrossMintConstants {
+	public static final String CHAIN = "solana";
+	public static final String FUNGIBILITY = "non-fungible";
+	public static final String MAIN_CURRENCY = "SOL";
+}
+
