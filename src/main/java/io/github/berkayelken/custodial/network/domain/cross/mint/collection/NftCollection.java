a0c708d9cbcb0394f9b827ae0e420d069b32d95b
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 13:55:41 +0300

    Create NftCollection.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/collection/NftCollection.java
@@ -0,0 +1,19 @@
+package io.github.berkayelken.custodial.network.domain.cross.mint.collection;
+
+import io.github.berkayelken.custodial.network.domain.cross.mint.Chain;
+import io.github.berkayelken.custodial.network.domain.cross.mint.CrossMintMetaData;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NftCollection {
+	private String id;
+	private CrossMintMetaData metadata;
+	private String fungibility;
+	private Chain onChain;
+	private int supplyLimit;
+	private CollectionPayment payments;
+}
