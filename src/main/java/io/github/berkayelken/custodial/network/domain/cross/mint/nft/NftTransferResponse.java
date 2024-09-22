5b3676b475e595f16f42e9cd35d390c7c277a07e
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 13:56:07 +0300

    Create NftTransferResponse.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/nft/NftTransferResponse.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.custodial.network.domain.cross.mint.nft;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NftTransferResponse {
+	private String message;
+	private String actionId;
+}
