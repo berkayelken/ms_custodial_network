ec19b3b636369e783a95f9409c3cb3dbb0d3028d
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 21:01:50 +0300

    Create CollectionCreatorController.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/rest/collection/CollectionCreatorController.java
@@ -0,0 +1,27 @@
+package io.github.berkayelken.custodial.network.rest.collection;
+
+import io.github.berkayelken.custodial.network.domain.cross.mint.CrossMintMetaData;
+import io.github.berkayelken.custodial.network.domain.cross.mint.collection.NftCollection;
+import io.github.berkayelken.custodial.network.extcall.collection.CollectionClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/artist/collection")
+public class CollectionCreatorController {
+	private final CollectionClient client;
+
+	public CollectionCreatorController(CollectionClient client) {
+		this.client = client;
+	}
+
+	@PostMapping
+	public NftCollection createCollection(@RequestBody CrossMintMetaData metadata, @RequestHeader String email,
+			@RequestParam String price, @RequestParam int supplyLimit) {
+		return client.createCollection(metadata, price, email, supplyLimit);
+	}
+}
