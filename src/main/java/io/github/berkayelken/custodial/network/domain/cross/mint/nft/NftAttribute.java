5bb80e7f56f23b2243109f1095deb6a84d69d4eb
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 13:56:00 +0300

    Create NftAttribute.java

---
+++ src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/nft/NftAttribute.java
@@ -0,0 +1,15 @@
+package io.github.berkayelken.custodial.network.domain.cross.mint.nft;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NftAttribute {
+	private String value;
+	@JsonProperty("trait_type")
+	private String traitType;
+}
