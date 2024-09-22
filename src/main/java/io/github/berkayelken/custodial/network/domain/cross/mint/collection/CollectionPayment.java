332dc8367f748a88c350d257f719616c9e395af6
Berkay Yelken <berkayelken@gmail.com>
2024-09-22 16:42:20 +0300

    CollectionPayment needed parameter consturctor is implemented

--- src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/collection/CollectionPayment.java
+++ src/main/java/io/github/berkayelken/custodial/network/domain/cross/mint/collection/CollectionPayment.java
@@ -1,14 +1,23 @@
 package io.github.berkayelken.custodial.network.domain.cross.mint.collection;
 
+import io.github.berkayelken.custodial.network.domain.cross.mint.CrossMintConstants;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @ToString
 public class CollectionPayment {
 	private String price;
 	private String recipientAddress;
 	private String currency;
+
+	public CollectionPayment(String recipientAddress, String price) {
+		this.price = price;
+		this.recipientAddress = recipientAddress;
+		this.currency = CrossMintConstants.MAIN_CURRENCY;
+	}
 }
